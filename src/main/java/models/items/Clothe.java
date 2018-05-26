e3fe8b061559b100f7385b9c116512172c0d03ed
AndrewLowrie <andrew2lowrie@googlemail.com>
2018-05-26 11:15:20 +0100

    Class rebuilding

--- src/main/java/models/items/Clothe.java
+++ src/main/java/models/items/Clothe.java
@@ -1,4 +1,36 @@
 package models.items;
 
-public class Clothe {
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "clothes")
+public class Clothe extends Item{
+
+    private String color;
+    private char size;
+
+    public Clothe() {
+    }
+
+    public Clothe(String color, char size) {
+        this.color = color;
+        this.size = size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public char getSize() {
+        return size;
+    }
+
+    public void setSize(char size) {
+        this.size = size;
+    }
 }
--- src/main/java/models/items/Electronic.java
+++ src/main/java/models/items/Electronic.java
@@ -1,4 +1,26 @@
 package models.items;
 
-public class Electronic {
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "electronics")
+public class Electronic extends Item {
+
+    private String parts;
+
+    public Electronic() {
+    }
+
+    public Electronic(String parts) {
+        this.parts = parts;
+    }
+
+    public String getParts() {
+        return parts;
+    }
+
+    public void setParts(String parts) {
+        this.parts = parts;
+    }
 }
