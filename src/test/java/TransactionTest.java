4b17283e90e4991068a62703af91fa93db50a955
AndrewLowrie <andrew2lowrie@googlemail.com>
2018-05-26 19:00:55 +0100

    Add get total from basket and method and test

--- src/test/java/TransactionTest.java
+++ src/test/java/TransactionTest.java
@@ -1,4 +1,6 @@
 import models.baskets.Basket;
+import models.items.Category;
+import models.items.Food;
 import models.transactions.Transaction;
 import models.users.LoyaltyCard;
 import models.users.User;
@@ -14,12 +16,15 @@ public class TransactionTest {
     Transaction transaction;
     LoyaltyCard loyaltyCard;
     Basket basket;
+    Basket testBasket;
+    Food food1;
+    Food food2;
 
     @Before
     public void before(){
         loyaltyCard = new LoyaltyCard(user, "na/na/na");
         user = new User(loyaltyCard, false, basket, "Andrew", "Fraz23", "pass123");
-        transaction = new Transaction(user, 50, "26/05/2018");
+        transaction = new Transaction(user, 0, "26/05/2018");
     }
 
     @Test
@@ -29,12 +34,22 @@ public class TransactionTest {
 
     @Test
     public void canGetAmount(){
-        assertEquals(50, transaction.getAmount(), 0.01);
+        assertEquals(0, transaction.getAmount(), 0.01);
     }
 
     @Test
     public void canGetDate(){
         assertEquals("26/05/2018", transaction.getDate());
+    }
+
+    @Test
+    public void canGetTotalFromBasket(){
+        testBasket = new Basket(user);
+        food1 = new Food(Category.FOOD, 3, 1.50, "Bananas");
+        food2 = new Food(Category.FOOD, 3, 1.50, "Apples");
+        testBasket.addItem(food1);testBasket.addItem(food2);
+        transaction.updateAmount(testBasket);
+        assertEquals(9, transaction.getAmount(), 0.01);
     }
 
 }
