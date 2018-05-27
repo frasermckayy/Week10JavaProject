1ba82cdbebf751e99260aa62458dff5fd4c71e0c
AndrewLowrie <andrew2lowrie@googlemail.com>
2018-05-27 12:27:48 +0100

    Add Stock Controller skeleton code

--- src/main/java/controllers/StockController.java
+++ src/main/java/controllers/StockController.java
@@ -1,4 +1,64 @@
 package controllers;
 
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+
 public class StockController {
+
+    public StockController(){
+        setUpEndPoints();
+    }
+
+    private void setUpEndPoints(){
+
+        get("/stock", (req, res) -> {
+            HashMap<String, Object> model = new HashMap<>();
+
+            // Data still to be added.
+
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+        get("/stock/:id", (req, res) -> {
+            HashMap<String, Object> model = new HashMap<>();
+
+            // Data still to be added.
+
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+        post("/stock", (req, res) -> {
+
+            // Data still to be added.
+
+            res.redirect("/stock");
+            return null;
+        });
+
+        post("/stock/edit", (req, res) -> {
+
+            // Data still to be added.
+
+            res.redirect("/stock");
+            return null;
+        });
+
+        post("/stock/delete", (req, res) -> {
+
+            // Data still to be added.
+
+            res.redirect("/stock");
+            return null;
+        });
+
+    }
+
+
+
+
 }
