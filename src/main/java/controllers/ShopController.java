91439f1626ba8e2a99598798104a7e4494f6faf9
AndrewLowrie <andrew2lowrie@googlemail.com>
2018-05-27 12:20:03 +0100

    Add shop controller skeleton code

--- src/main/java/controllers/ShopController.java
+++ src/main/java/controllers/ShopController.java
@@ -1,4 +1,58 @@
 package controllers;
 
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+
 public class ShopController {
+
+    public ShopController(){
+        setUpEndPoints();
+    }
+
+    private void setUpEndPoints(){
+
+        get("/shop", (req, res) -> {
+            HashMap<String, Object> model = new HashMap<>();
+
+            // Data still to be added.
+
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+        post("/shop", (req, res) -> {
+
+            // Data still to be added.
+
+            return null;
+        });
+
+        get("/shop/:id", (req, res) -> {
+            HashMap<String, Object> model = new HashMap<>();
+
+            // Data still to be added.
+
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+        get("/shop/search", (req, res) -> {
+            HashMap<String, Object> model = new HashMap<>();
+
+            // Data still to be added.
+
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+        post("/shop/search", (req, res) -> {
+
+            // Data still to be added.
+
+            return null;
+        });
+
+    }
 }
