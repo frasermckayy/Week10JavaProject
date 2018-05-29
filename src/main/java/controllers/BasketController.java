be35ccc876423eb54d94cb8ba6b2eb21d30aeb68
AndrewLowrie <andrew2lowrie@googlemail.com>
2018-05-29 11:57:00 +0100

    Add Basket with routes and links to edit/delete orders, functionality still needs work

--- src/main/java/controllers/BasketController.java
+++ src/main/java/controllers/BasketController.java
@@ -1,6 +1,12 @@
 package controllers;
 
 
+import db.DBHelper;
+import db.DBUser;
+import models.items.Clothe;
+import models.items.Electronic;
+import models.items.Food;
+import models.items.Item;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
@@ -19,27 +25,40 @@ public class BasketController {
 
         get("/basket", (req, res) ->{
          HashMap<String, Object> model = new HashMap<>();
-
-         //Needs data to be added.
-
-        return new ModelAndView(model, "template/layout.vtl");
+         model.put("user", DBUser.getUser(req, res));
+         model.put("template", "templates/basket/index.vtl");
+        return new ModelAndView(model, "templates/layout.vtl");
         }, new VelocityTemplateEngine());
 
-        get("/basket/:id", (req, res) -> {
+        get("/basket/food/:id", (req, res) -> {
             HashMap<String, Object> model = new HashMap<>();
-
-            //Needs data to be added.
-
+            int id = Integer.parseInt(req.params(":id"));
+            Food food = DBHelper.find(id, Food.class);
+            model.put("item", food);
+            model.put("user", DBUser.getUser(req, res));
+            model.put("template", "templates/basket/select.vtl");
             return new ModelAndView(model, "templates/layout.vtl");
         }, new VelocityTemplateEngine());
 
-        post("/basket", (req, res) -> {
+        get("/basket/clothes/:id", (req, res) -> {
+            HashMap<String, Object> model = new HashMap<>();
+            int id = Integer.parseInt(req.params(":id"));
+            Clothe clothe = DBHelper.find(id, Clothe.class);
+            model.put("item", clothe);
+            model.put("user", DBUser.getUser(req, res));
+            model.put("template", "templates/basket/select.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
 
-             //Needs data to be added.
-
-            res.redirect("/basket");
-            return null;
-        });
+        get("/basket/electronic/:id", (req, res) -> {
+            HashMap<String, Object> model = new HashMap<>();
+            int id = Integer.parseInt(req.params(":id"));
+            Electronic electronic = DBHelper.find(id, Electronic.class);
+            model.put("item", electronic);
+            model.put("user", DBUser.getUser(req, res));
+            model.put("template", "templates/basket/select.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
 
         post("/basket/edit", (req, res) -> {
 
@@ -49,17 +68,8 @@ public class BasketController {
             return null;
         });
 
-        post( "/basket/update", (req, res) ->{
-
-            // data needs to be added
-
-            res.redirect("/basket");
-            return null;
-        });
-
-        post("/basket/delete", (req, res) -> {
-
-            //Needs data to be added.
+        post("/basket/delete/:id", (req, res) -> {
+            int id = Integer.parseInt(req.queryParams(":id"));
 
             res.redirect("/basket");
             return null;
